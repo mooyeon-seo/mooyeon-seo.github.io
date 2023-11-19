b68054ebbb9703e4f02035932e67d1521426a723
Mooyeon Seo <mooyeon.seo@raspberrysherbet.ca>
2023-11-19 16:35:07 -0500

    implement block views for projects

--- src/containers/Projects.jsx
+++ src/containers/Projects.jsx
@@ -1,100 +1,169 @@
 import Title from "../components/Title";
-import ProjectSection from "../components/ProjectSection";
-import styled from "styled-components";
+import { motion } from "framer-motion";
+import { Typography } from "@mui/material";
+import { UI } from "../constants/ui";
+import { styled } from "@mui/system";
 
 const projects = [
   {
-    title: "WLP4 Compiler",
-    description: "lorem ipsum more text here",
+    title: "WLP4 Compiler ⭐",
+    category: "Academic (MIPS)",
+    description: "Compiler for WLP4, which is a strict subset of C++",
     link: "",
-    backgroundImage: {
-      src: "/assets/project/willitrain.png",
-      alt: "Will It Rain?",
-    },
-    images: [],
+    backgroundColor: "#980036",
   },
   {
-    title: "OS161",
-    description: "lorem ipsum more text here",
+    title: "OS/161 ⭐",
+    category: "Academic (C)",
+    description: "Implement Core Features like fork, execv and virtual memory",
     link: "",
-    backgroundImage: {
-      src: "/assets/project/willitrain.png",
-      alt: "Will It Rain?",
-    },
-    images: [],
+    backgroundColor: "#cf838d",
+  },
+  {
+    title: "MakeItEasy ⭐",
+    category: "Business Solution (iOS)",
+    description: "Streamlines the Process of Re-Tagging Items in Response to Frequent Promotions",
+    link: "https://apps.apple.com/us/app/basic-to-do/id1662248997",
+    backgroundColor: "#ef0056",
   },
   {
     title: "Will It Rain?",
+    category: "AppStore (iOS)",
     description:
-      "Tells you if it will rain in the next hour at your current location. It had been published to the app store ",
+      "Tells you if it will rain in the next hour at your current location",
     link: "https://apps.apple.com/ca/app/will-it-rain/id6443710105",
-    backgroundImage: {
-      src: "/assets/project/willitrain.png",
-      alt: "Will It Rain?",
-    },
-    images: [],
+    backgroundColor: "#ffcccc",
   },
+
   {
     title: "Basic To Do",
+    category: "AppStore (iOS)",
     description:
-      "Keeps track of your to do list. It had been published to the app store ",
+      "Support basic CRUD operations for notes with local persistance",
     link: "https://apps.apple.com/us/app/basic-to-do/id1662248997",
-    backgroundImage: {
-      src: "/assets/project/basicToDo.png",
-      alt: "Basic To Do",
-    },
-    images: [],
+    backgroundColor: "#bf0056",
   },
   {
     title: "Sentinel",
-    description:
-      "Prototype of parental control app, as part of an unpaid internship...",
+    category: "Volunteer Internship (iOS)",
+    description: "Prototype of Parental Control App",
     link: "",
-    backgroundImage: {
-      src: "/assets/project/willitrain.png",
-      alt: "Will It Rain?",
-    },
-    images: [],
-  },
-  {
-    title: "This Website",
-    description:
-      "My personal website, built with React, Material UI and framer-motion. It is hosted on Github Pages.",
-    link: "",
+    backgroundColor: "#991476",
   },
   {
     title: "Note Taking App",
-    description: "lorem ipsum more text here",
+    category: "Academic (Android)",
+    description:
+      "Support basic CRUD operations for notes with local persistance",
     link: "",
-    backgroundImage: {
-      src: "/assets/project/willitrain.png",
-      alt: "Will It Rain?",
-    },
-    images: [],
+    backgroundColor: "#ffccdd",
   },
   {
     title: "Battleship",
-    description: "lorem ipsum more text here",
+    category: "Academic (Android)",
+    description: "A Classic Battleship Board Game with User Friendly UI",
     link: "",
-    backgroundImage: {
-      src: "/assets/project/willitrain.png",
-      alt: "Will It Rain?",
-    },
-    images: [],
+    backgroundColor: "#e099dd",
+  },
+  {
+    title: "Custom Exploit Scripts",
+    category: "Academic (C)",
+    description:
+      "Gain Superuser Access Into An Old Operating System Back From Early 2000s",
+    link: "",
+    backgroundColor: "#ee99dd",
+  },
+  {
+    title: "Sentimental Mirror",
+    category: "Machine Learning (iOS)",
+    description:
+      "Server That Analyzes Your Messages And Posts Them on X (Twitter) With Your Sentiment Score",
+    link: "",
+    backgroundColor: "#ee44ff",
   },
 ];
 
-const ProjectsContainer = styled("div")({
-    height: "100%",
+const ProjectSection = styled("div")({
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
 });
+
+const ProjectContent = styled("div")({
+  height: "100%",
+  display: "grid",
+  gridTemplateColumns: `${UI.businessBoxUnit} ${UI.businessBoxUnit} ${UI.businessBoxUnit}`, // Three equal-width columns
+  gridTemplateRows: `${UI.businessBoxUnit} ${UI.businessBoxUnit} ${UI.businessBoxUnit}`, // Three equal-width columns
+  alignItems: "center",
+  justifyContent: "center",
+  gap: UI.gap,
+  padding: UI.padding,
+});
+
+const ProjectTextContainer = styled("div")({
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  justifyContent: "start",
+  padding: UI.padding,
+  gap: UI.gap,
+  color: "white",
+});
+
+const DescriptionBlock = ({ project }) => {
+  const Content = styled("div")({
+    backgroundColor: project.backgroundColor
+      ? project.backgroundColor
+      : "black",
+    borderRadius: UI.borderRadius,
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "start",
+    height: "100%",
+    width: "100%",
+    gap: UI.gap,
+  });
+  return (
+    <motion.div
+      whileHover={{ scale: 1.025 }}
+      initial={{ x: 50 }}
+      whileInView={{ x: 0 }}
+      transition={{ duration: 1.2 }}
+      style={{ height: "100%", width: "100^" }}
+    >
+      <Content>
+        {project.title && (
+          <ProjectTextContainer>
+            <Typography
+              variant="h6"
+              align="center"
+              color="white"
+              fontFamily={"anton"}
+            >
+              {project.title}
+            </Typography>
+            <Typography variant="subtitle1">{project.category}</Typography>
+            <Typography variant="subtitle1">{project.description}</Typography>
+          </ProjectTextContainer>
+        )}
+      </Content>
+    </motion.div>
+  );
+};
 
 export default function Projects() {
   return (
-    <ProjectsContainer>
+    <ProjectSection>
       <Title title="Project" />
-      <div>
-        <ProjectSection category="" projects={projects} />
-      </div>
-    </ProjectsContainer>
+      <Typography variant="subtitle1">
+        
+      </Typography>
+      <ProjectContent>
+        {projects.map((project) => (
+          <DescriptionBlock project={project} />
+        ))}
+      </ProjectContent>
+    </ProjectSection>
   );
 }
