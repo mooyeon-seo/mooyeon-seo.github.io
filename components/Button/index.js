fb6169795e9885833d4e1761d238398d6acbf5ea
mooyeon-seo <seomooyeon@gmail.com>
2025-01-05 17:31:43 +0900

    fix: adjust padding for Button component for better spacing

--- components/Button/index.js
+++ components/Button/index.js
@@ -9,7 +9,7 @@ const Button = ({ children, type, onClick, classes }) => {
       <button
         onClick={onClick}
         type="button"
-        className={`text-sm tablet:text-base p-1 laptop:p-2 m-1 laptop:m-2 rounded-lg ${
+        className={`text-sm tablet:text-base p-1 laptop:p-3 m-1 laptop:m-3 rounded-lg ${
           theme === "dark" ? "bg-white text-black" : "bg-black text-white"
         }  transition-all duration-300 ease-out first:ml-0  hover:-translate-y-0.5 active:scale-100 link ${
           data.showCursor && "cursor-none"
@@ -27,7 +27,7 @@ const Button = ({ children, type, onClick, classes }) => {
         theme === "dark"
           ? "hover:bg-slate-600 text-white"
           : "hover:bg-slate-100"
-      } hover:-translate-y-0.5 active:scale-100 tablet:first:ml-0 p-1  ${
+      } hover:-translate-y-0.5 active:scale-100 tablet:first:ml-0 ${
         data.showCursor && "cursor-none"
       } ${classes} link`}
     >
